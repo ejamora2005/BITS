2f461b6b9b6695946191d9ab7bb369dc7706cc2f
Emil Jon Amora <ejamora19@gmail.com>
2026-08-25 08:27:26 +0800

    Add BITS favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Bontoc Information Technology Society</title>
+    <link rel="icon" href="{{ asset('images/bits.png') }}" type="image/png">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=poppins:400,500,600,700,800,900" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
